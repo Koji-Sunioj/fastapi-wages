6051cfdf96ce632e8f4660fd2922c7bf19a5dda3
Koji-Sunioj <koji.gabriel218@gmail.com>
2023-07-22 01:36:37 +0300

    token

--- main.py
+++ main.py
@@ -1,39 +1,61 @@
 from fastapi import FastAPI
+from datetime import datetime, timedelta
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from passlib.context import CryptContext
-from dotenv import load_dotenv
+from dotenv import dotenv_values
+from jose import JWTError, jwt
 import os
 import psycopg2
 
-load_dotenv() 
-app = FastAPI()
-conn = psycopg2.connect(database="wages",
-                        host="localhost",
-                        user="my_user",
-                        password="root",
-                        port="5432")
+
+app=FastAPI()
+envs=dict(dotenv_values(".env"))
+conn= psycopg2.connect(database=envs["DB_NAME"],
+                        host=envs["HOST"],
+                        user=envs["USER"],
+                        password=envs["PWD"],
+                        port=envs["PORT"])
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class User(BaseModel):
     username: str
     password: str
+
+def one_to_dict(cursor):
+    values = list(cursor.fetchone())
+    columns = [column[0] for column in cursor.description]
+    pointers = {column:value for column,value in zip(columns,values)}
+    return pointers
+
+@app.post("/sign-in/")
+async def get_token(user: User):           
+    cursor = conn.cursor()
+    command = "select username,password from wages_users where username='%s'" % (user.username)
+    cursor.execute(command)
+    existing_user = one_to_dict(cursor)
+    verified = pwd_context.verify(user.password, existing_user["password"])
+    if verified:
+        now = datetime.utcnow()
+        expires = now + timedelta(minutes=60)
+        jwt_payload = {"sub":existing_user["username"],"iat":now,"exp":expires}
+        token = jwt.encode(jwt_payload,envs["SECRET"])
+        return {"token":token}
+    else:
+        return JSONResponse(status_code=403,content={"message": "invalid credentials"})
+
     
 @app.get("/users/{user_id}")
 async def get_user(user_id:int):
-   
     try:
         cursor = conn.cursor()
-        command = "select user_id,username from wages_users where user_id = %s" %(user_id) 
+        command = "select user_id,username from wages_users where user_id=%s;" %(user_id) 
         cursor.execute(command)
-        values = list(cursor.fetchone())
-        columns = [column[0] for column in cursor.description]
-        user = {column:value for column,value in zip(columns,values)}
-        cursor.close()
-        conn.close()
+        user = one_to_dict(cursor)
         return {"user": user}
-    except:
+    except Exception as error:
+        print(error)
         return JSONResponse(status_code=404,content={"message": "user not found"})
 
 @app.post("/users/")
@@ -43,6 +65,7 @@ async def create_user(user: User):
         command = "insert into wages_users (username,password) values ('%s','%s')" % (user.username,pwd_context.hash(user.password))
         cursor.execute(command)
         conn.commit()
-        return user
-    except:
+        return JSONResponse(status_code=200,content={"message": "successfully created user"})
+    except Exception as error:
+        print(error)
         return JSONResponse(status_code=409,content={"message": "user already exists"})
